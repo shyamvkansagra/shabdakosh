3283c7e7e3b481c4f9c82d757c35c39a84aa49ef
Shyam Kansagra <shyam@mgrm.tech>
2018-10-30 01:26:58 +0530

    Add DB initial file and dictionary to repo

--- src/Components/SearchBox.js
+++ src/Components/SearchBox.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 import WordDescription from './WordDescription';
+import setDB from '../db/setDB';
 import './style.css';
 
 export default class SearchBox extends Component {
+    componentDidMount() {
+        const isDBSet = window.localStorage.getItem('isDBSet');
+        if (!isDBSet) {
+            const result = setDB();
+            if (result === 'success') {
+                window.localStorage.setItem('isDBSet', true);
+            }
+        }
+    }
+
     render() {
         return (
             <div>
                 <div className="searchContainer">
                     <span className="labelMsg">
-                        <p>Want to know the meaning of that cool word you just heard? Type below:</p>
+                        <p>Want to know the meaning of that cool word you came across? Type below:</p>
                     </span>
                     <div className="searchBox">
                         <input id="search" name="Search" placeholder="Your text goes here..." type="text" />
